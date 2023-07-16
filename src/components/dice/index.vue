2a3ee2acde4c44d2db56a141399b4cce71347b98
Atip Nasakun <71032885+ATIPPAz@users.noreply.github.com>
2023-07-16 13:39:36 +0700

    Update index.vue

--- src/components/dice/index.vue
+++ src/components/dice/index.vue
@@ -1,31 +1,46 @@
 <template>
-  <div class="dice" @click="select">
+  <div class="dice" @click="select" :class="{ cursor: props.value }" :style="color">
     {{ props.value }}
   </div>
 </template>
 <script setup lang="ts">
 import { useDiceStore } from '@/stores/dice'
-const { setDiceValue } = useDiceStore()
-const props = defineProps<{
-  isMove: boolean
-  value: number
-}>()
+import { DiceNumber, DiceColor } from '@/enum/board'
+import { computed } from 'vue'
+
+const { setDiceValue, setDiceFloat, resetDiceFloat } = useDiceStore()
+const props = withDefaults(
+  defineProps<{
+    isMove?: boolean
+    value: DiceNumber | null
+    color: DiceColor | null
+  }>(),
+  {
+    isMove: false,
+    color: null,
+    value: null
+  }
+)
+
+const color = computed(() => `background-color:${props.color}`)
 
 function select() {
   if (props.isMove) {
-    setDiceValue(props.value)
+    setDiceFloat()
+    setDiceValue(props.value!)
   }
 }
 </script>
 <style scoped>
 .dice {
-  width: 50px;
-  height: 50px;
-  border: 1px solid red;
+  width: 48px;
+  height: 48px;
   border-radius: 5px;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+.cursor {
   cursor: pointer;
 }
 </style>
