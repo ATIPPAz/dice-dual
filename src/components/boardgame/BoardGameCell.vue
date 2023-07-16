4b90cdbbc6e557a796a2bfa9e797f731f2c37e9c
Atip Nasakun <71032885+ATIPPAz@users.noreply.github.com>
2023-07-16 13:39:53 +0700

    Update BoardGameCell.vue

--- src/components/boardgame/BoardGameCell.vue
+++ src/components/boardgame/BoardGameCell.vue
@@ -1,25 +1,40 @@
 <template>
-  <div class="board-game-cell" :class="{ cursor: hasValue() }" @click="addDiceValue">
-    {{ value || '' }}
-  </div>
+  <Dice
+    class="board-game-cell"
+    :class="{ cursor: hasValue() }"
+    :style="colorDisplay"
+    @click="addDiceValue"
+    :color="color"
+    :value="value"
+  />
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+import Dice from '@/components/dice/index.vue'
+import { ref, computed } from 'vue'
 import { useDiceStore } from '@/stores/dice'
 import { storeToRefs } from 'pinia'
 import { Board } from '@/enum/board'
-const { resetDiceValue, hasValue, setReturnDice } = useDiceStore()
-const { diceValue } = storeToRefs(useDiceStore())
-const value = ref<null | number>(null)
+import { DiceNumber, DiceColor } from '@/enum/board'
+
+const { resetDiceValue, hasValue, resetDiceFloat, setReturnDice } = useDiceStore()
+const { diceValue, colorDice } = storeToRefs(useDiceStore())
+const value = ref<null | DiceNumber>(null)
+const color = ref<null | DiceColor>(null)
 const props = defineProps<{ boardHolder: Board }>()
+const colorDisplay = computed(() => `background-color:${color.value}`)
 function addDiceValue() {
-  if (hasValue() && props.boardHolder === Board.MY_BOARD) {
+  if (hasValue() && props.boardHolder === Board.MY_BOARD && value.value === null) {
     value.value = diceValue.value
-    resetDiceValue()
+    color.value = colorDice.value
   } else if (hasValue() && props.boardHolder === Board.ENEMY_BOARD) {
     alert('not your board')
     setReturnDice()
+  } else if (hasValue()) {
+    alert('have value')
+    setReturnDice()
   }
+  resetDiceValue()
+  resetDiceFloat()
 }
 </script>
 <style scoped>
